7aaef76413d2f7f3469299ce2f7ca96689021d20
Wildrimak <wildrimak@gmail.com>
2023-11-03 22:12:16 -0300

    finished cap. 4 aula 6

---
+++ funcional/first_class_function.js
@@ -0,0 +1,13 @@
+// Diz-se que uma linguagem de programacao possui
+// funcoes de primeira classe quando funcoes nessa 
+// linguagem sao tratadas como qualquer outra variavel.
+
+const x = 3
+const y = function(txt) {
+    return `Esse é o texto: ${txt}`;
+}
+const z = () => console.log('Zzzzzzz!');
+
+console.log(x)
+console.log(y('Olá'))
+z()
---
+++ funcional/higher_order_function.js
@@ -0,0 +1,23 @@
+// Funcoes que operam em outras funcoes,
+// tomando-as como argumentos ou retornado-as,
+// sao chamadas de higher-order functions
+
+function executar(fn, ...params) {
+    return function (textoInicial) {
+        return `${textoInicial} ${fn(...params)}!`
+    }
+}
+
+function somar(a, b, c) {
+    return a + b + c
+}
+
+function multi(a, b) {
+    return a * b
+}
+
+const r1 = executar(somar, 4, 5, 6)('O resultado da soma é')
+const r2 = executar(multi, 30, 40)('O resultado da multiplicacao é')
+
+console.log(r1);
+console.log(r2);
